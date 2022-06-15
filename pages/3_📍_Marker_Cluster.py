c4ecbe48140ff01f778a989936707f9b6b6a38d2
ckyriakos <45677715+ckyriakos@users.noreply.github.com>
2022-06-15 22:28:42 +0300

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -19,14 +19,15 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[40, -100], zoom=4)
-        cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
-        regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
-
-        m.add_geojson(regions, layer_name='US Regions')
+        #cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
+        #regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
+        cities = "https://raw.githubusercontent.com/ckyriakos/thesis_front_end/master/data/greek_cities.csv"
+        regions = "https://raw.githubusercontent.com/ckyriakos/thesis_front_end/master/data/greek_cities.geojson"
+        m.add_geojson(regions, layer_name='Greek Regions')
         m.add_points_from_xy(
             cities,
-            x="longitude",
-            y="latitude",
+            x="lng",
+            y="lat",
             color_column='region',
             icon_names=['gear', 'map', 'leaf', 'globe'],
             spin=True,
